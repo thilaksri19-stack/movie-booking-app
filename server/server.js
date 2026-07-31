149e713e8b35309fe877acd883690926b99fa812
Thilaksri <thilaksri19@gmail.com>
2026-07-31 20:08:37 +0530

    Bind Express to 0.0.0.0

--- server/server.js
+++ server/server.js
@@ -36,6 +36,6 @@ app.get("/", (req, res) => {
 // Start Server
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
