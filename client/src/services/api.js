061720719ad8e90d7b7180cba25c19573e0e5c37
Thilaksri <thilaksri19@gmail.com>
2026-07-31 20:24:36 +0530

    Connect frontend to Railway backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+export default axios.create({
+  baseURL: "https://movie-booking-app-production-91bb.up.railway.app/api",
 });
-
-export default API;
