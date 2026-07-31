001538e6517cf5c0ae98c69ac6b2fc45c4098605
Thilaksri <thilaksri19@gmail.com>
2026-07-31 20:11:18 +0530

    Add health endpoint

--- server/server.js
+++ server/server.js
@@ -39,3 +39,10 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "OK",
+    message: "Backend is working",
+  });
+});
